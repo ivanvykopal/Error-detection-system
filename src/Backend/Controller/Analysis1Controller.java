3d74051f41a31e57f112478c212c072fd7cb44e2
Ivan Vykopal <ivan.vykopal@gmail.com>
2021-01-27 19:54:43 +0100

    Adding a Analysis1Controller class

---
+++ src/Backend/Controller/Analysis1Controller.java
@@ -0,0 +1,49 @@
+package Backend.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
+public class Analysis1Controller {
+    String file;
+
+    @FXML
+    private Label warning;
+
+    @FXML
+    public void getFile(ActionEvent event) {
+        FileChooser filechooser = new FileChooser();
+        filechooser.setTitle("Vyberte súbor");
+        filechooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("C kód", "*.c"));
+
+        File selectedFile = filechooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            warning.setTextFill(Color.web("#000000"));
+            warning.setText("Súbor: " + selectedFile.getAbsolutePath());
+            file = selectedFile.getAbsolutePath();
+        } else {
+            warning.setTextFill(Color.web("#FF0000"));
+            warning.setText("Chybný súbor!");
+            file = null;
+        }
+    }
+
+    public void analyzeCode(ActionEvent event) {
+        Alert warning = new Alert(Alert.AlertType.WARNING);
+        if (file == null) {
+            warning.setContentText("Nie je vybraný súbor alebo vybraný súbor je chybný!");
+            warning.setHeaderText("Nesprávny súbor!");
+            warning.setTitle("Upozornenie");
+            warning.show();
+        }
+        else {
+            System.out.println("Analyzujem kód!");
+        }
+    }
+}

4d0715441cf07be160054badea64dcfd05a0d54a
Ivan Vykopal <ivan.vykopal@gmail.com>
2021-01-30 16:25:28 +0100

    Class Record: constructor, getters and setters were added

--- src/Compiler/SymbolTable/Record.java
+++ src/Compiler/SymbolTable/Record.java
@@ -16,7 +16,67 @@ public class Record {
     private ArrayList<String> parameters;
     private int size;
 
-    public Record() {
-
+    public Record(byte type, int line, String value, byte kind) {
+        this.type = type;
+        this.declarationLine = line;
+        this.declarationValue = value;
+        this.kind = kind;
     }
+
+    public byte getType() {
+        return type;
+    }
+
+    public void setType(byte type) {
+        this.type = type;
+    }
+
+    public int getDeclarationLine() {
+        return declarationLine;
+    }
+
+    public void setDeclarationLine(int declarationLine) {
+        this.declarationLine = declarationLine;
+    }
+
+    public String getDeclarationValue() {
+        return declarationValue;
+    }
+
+    public void setDeclarationValue(String declarationValue) {
+        this.declarationValue = declarationValue;
+    }
+
+    public int getFirstUsage() {
+        return firstUsage;
+    }
+
+    public void setFirstUsage(int firstUsage) {
+        this.firstUsage = firstUsage;
+    }
+
+    public byte getKind() {
+        return kind;
+    }
+
+    public void setKind(byte kind) {
+        this.kind = kind;
+    }
+
+    public ArrayList<String> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(ArrayList<String> parameters) {
+        this.parameters = parameters;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
 }

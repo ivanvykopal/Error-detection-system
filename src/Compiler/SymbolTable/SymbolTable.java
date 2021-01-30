01f4c1992e3c27f006a89bc7b4d2ced13221f073
Ivan Vykopal <ivan.vykopal@gmail.com>
2021-01-30 16:40:27 +0100

    Class SymbolTable: lookup, insert, free and setValue were added

--- src/Compiler/SymbolTable/SymbolTable.java
+++ src/Compiler/SymbolTable/SymbolTable.java
@@ -7,13 +7,32 @@ import java.util.HashMap;
  */
 public class SymbolTable {
     SymbolTable parent = null;
-    HashMap table;
+    HashMap<String, Record> table;
     /**
      * Konštruktor, v ktorom nastavujeme predchádzajúcu tabuľku.
      * @param parent rodičovská tabuľka
      */
     public SymbolTable(SymbolTable parent) {
         this.parent = parent;
-        table = new HashMap();
+        table = new HashMap<>();
+    }
+
+    public Record lookup(String key) {
+        return table.get(key);
+    }
+
+    public void insert(String key, Record value) {
+        table.put(key, value);
+    }
+
+    public void free() {
+        table.clear();
+    }
+
+    public void setValue(String key, Record newValue) {
+        Record item = table.replace(key, newValue);
+        if (item == null) {
+            table.put(key, newValue);
+        }
     }
 }

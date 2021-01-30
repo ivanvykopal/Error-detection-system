42b7aa9ac4cb8e03451c95f18bb0f82bccae4bb2
Ivan Vykopal <ivan.vykopal@gmail.com>
2021-01-30 11:28:17 +0100

    Adding a Kind class

---
+++ src/Compiler/SymbolTable/Kind.java
@@ -0,0 +1,8 @@
+package Compiler.SymbolTable;
+
+public class Kind {
+    public static byte VARIABLE;
+    public static byte ARRAY;
+    public static byte FUNCTION;
+    public static byte PARAMETER;
+}
